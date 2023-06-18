df6e8020542f3ad72c3365b9171a54d930a5387b
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-06-18 18:10:19 +0900

    add: css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,6 @@ export default defineNuxtConfig({
   css: [
     "vuetify/lib/styles/main.sass",
     "mdi/css/materialdesignicons.min.css",
-    // "/assets/style/style.css",
+    "~/assets/css/style.css",
   ],
 });
