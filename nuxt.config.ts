452f2d9bb3197f01bdbb841a44b5ab7a8d4cf366
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-06-18 19:15:20 +0900

    add: vercel/analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
   srcDir: "src",
   build: {
     transpile: ["vuetify"],
