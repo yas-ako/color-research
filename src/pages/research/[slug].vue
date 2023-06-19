09fc4291beec394995756064b28c82601f31ffb6
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-06-20 07:07:07 +0900

    update: 色のボタン

--- src/pages/research/[slug].vue
+++ src/pages/research/[slug].vue
@@ -3,8 +3,14 @@
   <p>
     <DisplayColor :id="currentNumber" />
   </p>
-  <div @click="currentNumber = currentNumber + 1">+</div>
-  <div @click="currentNumber = currentNumber - 1">-</div>
+  <!-- <div @click="currentNumber = currentNumber + 1">+</div> -->
+  <!-- <div @click="currentNumber = currentNumber - 1">-</div> -->
+  <v-btn @click="currentNumber = currentNumber + 1" class="bg-primary ma-6"
+    >+</v-btn
+  >
+  <v-btn @click="currentNumber = currentNumber - 1" class="bg-primary ma-6"
+    >-</v-btn
+  >
   {{ currentNumber }}
 </template>
 
