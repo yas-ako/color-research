ac081b923d29f58c6b9fc177565fd219adce9c63
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-06-19 18:42:13 +0900

    feat: ReaserchLink コンポーネント タイトル，表示するテキスト，リンクを受け取る

---
+++ src/components/ResearchLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card :title="title" :text="text" :to="link" class="bg-secondary my-7" />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  text: string;
+  link: string;
+}
+
+const Props = withDefaults(defineProps<Props>(), {
+  title: "titlea",
+  text: "texta",
+  link: "linka",
+});
+</script>
