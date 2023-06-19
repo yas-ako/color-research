a641b1f127679eef8839fe1135afa1b6f87897ae
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-06-20 06:59:32 +0900

    add: [slug].vue 現在の番号をDisplayColorに代入

---
+++ src/pages/research/[slug].vue
@@ -0,0 +1,13 @@
+<template>
+  aaa
+  <p>
+    <DisplayColor :id="currentNumber" />
+  </p>
+  <div @click="currentNumber = currentNumber + 1">+</div>
+  <div @click="currentNumber = currentNumber - 1">-</div>
+  {{ currentNumber }}
+</template>
+
+<script setup>
+const currentNumber = ref(0);
+</script>
