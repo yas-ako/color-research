d35b8e697968db81a723095d735bc6924a24502a
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-06-20 06:59:51 +0900

    chore: 不要なコメントアウトを削除

--- src/app.vue
+++ src/app.vue
@@ -7,44 +7,21 @@
       <v-app-bar-nav-icon
         variant="text"
         @click.stop="isMenuOpen = !isMenuOpen"
-      ></v-app-bar-nav-icon>
+      />
     </v-app-bar>
     <v-navigation-drawer v-model="isMenuOpen" location="right" temporary>
       <v-container>
-        <!-- <v-btn variant="flat" color="secondary" prepend-icon="mdi-login" class="my-2" to="/signin">
-          ログイン
-        </v-btn> -->
-        <!-- <v-btn variant="flat" color="secondary" prepend-icon="mdi-login" class="my-2" to="/dashboard">
-          アカウントページ
-        </v-btn> -->
         <v-btn variant="flat" color="secondary" class="my-2" to="/about">
-          <!-- prepend-icon="mdi-login" -->
           この調査について
         </v-btn>
-        <!-- <v-progress-circular indeterminate color="red"></v-progress-circular> -->
       </v-container>
     </v-navigation-drawer>
     <v-main>
-      <v-main>
-        <v-container>
-          <NuxtPage />
-        </v-container>
-      </v-main>
+      <v-container>
+        <NuxtPage />
+      </v-container>
     </v-main>
   </v-app>
-  <!-- </template> -->
-
-  <!-- <script setup>
-
-
-const drawer = ref(false);
-
-function logout() {
-  const client = useSupabaseAuthClient()
-  client.auth.signOut()
-  drawer.value = false;
-}
-</script> -->
 </template>
 
 <script setup>
