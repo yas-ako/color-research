ba33e97dcb8329aaeb0e82a052c9381eeeeb6754
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-06-19 18:42:33 +0900

    add: 調査の一覧を表示

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,23 @@
-<template>aa</template>
+<template>
+  <template v-for="list in researchList">
+    <ResearchLink :link="list.link" :title="list.title" :text="list.text" />
+  </template>
+</template>
 
 <script setup>
+const researchList = [
+  {
+    link: "/research/1",
+    title: "調査①",
+    text: "色の見分け安さ",
+  },
+  {
+    link: "/research/2",
+    title: "調査②",
+    text: "色の心地よさ",
+  },
+];
+
 definePageMeta({
   middleware: [
     function (to, from) {
