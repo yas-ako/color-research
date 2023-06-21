93fdcf6b2b1d2f390934a2939266ca20d37f85c3
yasako <105139975+yas-ako@users.noreply.github.com>
2023-06-21 08:37:14 +0000

    update: アンケートに回答するボタン

---
+++ src/components/SurveyButtonIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn :value="id" class="text-h5">
+    <!-- <v-icon>mdi-history</v-icon> -->
+    {{ displayText }}
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  id: number;
+  displayText: string;
+}
+const Props = defineProps<Props>();
+</script>
