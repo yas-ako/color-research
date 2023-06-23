2e730c4395f91bbcef98fcf0831a249a2e5a2079
yasako <105139975+yas-ako@users.noreply.github.com>
2023-06-23 14:30:20 +0000

    update: ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
   srcDir: "src",
   build: {
