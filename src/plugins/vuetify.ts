a8724fc8d38c6ee8e498ad778dea0270211782f6
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-08-06 21:09:26 +0900

    fix: フォントの修正

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,3 +1,4 @@
+import "@/assets/css/main.scss";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
@@ -7,19 +8,19 @@ export default defineNuxtPlugin((nuxtApp) => {
     components,
     theme: {
       themes: {
-          light: {
-              colors: {
-                  primary: '#1976D2',
-                  secondary: '#424242',
-                  accent: '#82B1FF',
-                  error: '#FF5252',
-                  info: '#2196F3',
-                  success: '#4CAF50',
-                  warning: '#FFC107',
-              },
+        light: {
+          colors: {
+            primary: "#1976D2",
+            secondary: "#424242",
+            accent: "#82B1FF",
+            error: "#FF5252",
+            info: "#2196F3",
+            success: "#4CAF50",
+            warning: "#FFC107",
           },
+        },
       },
-  },
+    },
     directives,
   });
 
